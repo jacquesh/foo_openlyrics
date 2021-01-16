807318469e4c1e3de0a5749282c6a42085dd42d2
Jacques Heunis <git@jacquesheunis.com>
2021-01-16 10:38:53 +0000

    Add libxml

---
+++ 3rdparty/libxml2-2.9.10/win32/configure.bat
@@ -0,0 +1,3 @@
+REM NOTE (jacquesh): This is not part of the usual libxml2 distribution, but exists to track the configuration options used in this particular repo
+
+cscript configure.js legacy=no ftp=no http=no sax1=no schematron=no schemas=no
---
+++ 3rdparty/libxml2-2.9.10/win32/rcVersion.h
@@ -0,0 +1,9 @@
+/*
+  rcVersion.h
+  This file is generated automatically by configure.js.
+*/
+
+#define LIBXML_MAJOR_VERSION 2
+#define LIBXML_MINOR_VERSION 9
+#define LIBXML_MICRO_VERSION 10
+#define LIBXML_DOTTED_VERSION "2.9.10"
