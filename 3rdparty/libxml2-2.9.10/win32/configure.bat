e545e701e77ccfbba6e7e0aa8e1e4e0d110cce7b
Jacques Heunis <git@jacquesheunis.com>
2021-03-28 11:12:13 +0100

    Add a build step for configuring the libxml2 build before building

--- 3rdparty/libxml2-2.9.10/win32/configure.bat
+++ 3rdparty/libxml2-2.9.10/win32/configure.bat
@@ -1,3 +1,5 @@
 REM NOTE (jacquesh): This is not part of the usual libxml2 distribution, but exists to track the configuration options used in this particular repo
 
+pushd "%~dp0"
 cscript configure.js legacy=no ftp=no http=no sax1=no schematron=no schemas=no c14n=no catalog=no docb=no xptr=no xinclude=no iconv=no xml_debug=no regexps=no modules=no reader=no writer=no walker=no pattern=no push=no valid=no output=yes
+popd
