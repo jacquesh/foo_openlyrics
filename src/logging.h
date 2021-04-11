abc6f157213f4dffd4726ebfaeba9839378e587f
Jacques Heunis <git@jacquesheunis.com>
2021-04-11 17:04:45 +0100

    Fix the callstack tracker objects causing name clashes in release builds

--- src/logging.h
+++ src/logging.h
@@ -4,7 +4,10 @@
 #define LOG_WARN(MSG, ...) console::printf("WARN-OpenLyrics: " MSG, ##__VA_ARGS__)
 
 #ifdef NDEBUG
-#define LOG_INFO(MSG, ...) TRACK_CALL_TEXT(MSG)
+// NOTE: This is effectively the same as `TRACK_CALL_TEXT(MSG)` except multiple statements in the same scope do not cause a name clash
+#define LOG_INFO_TRACKER_CONCAT(prefix, suffix) prefix##suffix
+#define LOG_INFO_TRACKER_NAME(id) LOG_INFO_TRACKER_CONCAT(TRACKER_, id)
+#define LOG_INFO(MSG, ...) uCallStackTracker LOG_INFO_TRACKER_NAME(__LINE__)(#MSG)
 #else // NDEBUG
 #define LOG_INFO(MSG, ...) console::printf("INFO-OpenLyrics: " MSG, ##__VA_ARGS__)
 #endif // NDEBUG
