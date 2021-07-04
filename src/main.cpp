e2815377a908e054c428114689b8e384f86d1441
Jacques Heunis <git@jacquesheunis.com>
2021-07-04 16:31:47 +0100

    Bump version number to v0.7

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 
-#define OPENLYRICS_VERSION "0.7-dev"
+#define OPENLYRICS_VERSION "0.7"
 
 // Declaration of your component's version information
 // Since foobar2000 v1.0 having at least one of these in your DLL is mandatory to let the troubleshooter tell different versions of your component apart.
@@ -14,7 +14,7 @@ DECLARE_COMPONENT_VERSION(
     "Source available at https://github.com/jacquesh/foo_openlyrics\n"
     "\n"
     "Changelog:\n"
-    "Version 0.7-dev (" __DATE__ "):\n"
+    "Version 0.7 (2021-07-04):\n"
     "- Add a source for QQ Music\n"
     "- Add a source for darklyrics.com\n"
     "- Add an auto-edit for removing duplicate blank lines\n"
