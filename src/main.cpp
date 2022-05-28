110f7c7d2701d9723404489aa7f7b6091c86496f
Jacques Heunis <git@jacquesheunis.com>
2022-05-28 13:50:24 +0100

    Finalise v1.2

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "stdafx.h"
 
-#define OPENLYRICS_VERSION "1.2-dev"
+#define OPENLYRICS_VERSION "1.2"
 
 // Declaration of your component's version information
 // Since foobar2000 v1.0 having at least one of these in your DLL is mandatory to let the troubleshooter tell different versions of your component apart.
@@ -27,7 +27,7 @@ void OpenLyricsVersion::get_about_message(pfc::string_base & out)
     out += "Source available at https://github.com/jacquesh/foo_openlyrics\n";
     out += "You can support development at https://www.buymeacoffee.com/jacquesheunis\n";
     out += "\nChangelog:\n";
-    out += "Version " OPENLYRICS_VERSION " (" __DATE__ "):\n"
+    out += "Version 1.2 (2022-05-28):\n"
     "- Add some additional info to the debug logs for searching & saving\n"
     "- Fix failing to load the last line of lyrics\n"
     "- Fix empty lines being ignored by the editor\n"
