880f11c762a2bdf35a5c9c7c7271d24399db5daa
Jacques Heunis <git@jacquesheunis.com>
2023-06-04 06:54:47 +0100

    Add stb_image and stb_image_resize for handling (album) art backgrounds

--- src/PCH.cpp
+++ src/PCH.cpp
@@ -1,1 +1,15 @@
 #include "stdafx.h"
+
+#pragma warning(push, 0)
+#define STBI_NO_PSD
+#define STBI_NO_TGA
+#define STBI_NO_GIF
+#define STBI_NO_HDR
+#define STBI_NO_PIC
+#define STBI_NO_PNM
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+#define STB_IMAGE_RESIZE_IMPLEMENTATION
+#include "stb_image_resize.h"
+#pragma warning(pop)
