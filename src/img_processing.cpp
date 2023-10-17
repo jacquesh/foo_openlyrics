6e15c71a2a17e213bac5d6ba6bc62e8d5455d4df
Jacques Heunis <git@jacquesheunis.com>
2023-10-17 20:33:05 +0100

    Introduce libdivide and optimise image blur function
    In my testing the optimised version took about 25% as long as the
    original code (test run took ~80ms instead of ~340ms)

--- src/img_processing.cpp
+++ src/img_processing.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 
 #pragma warning(push, 0)
+#include "libdivide.h"
 #include "stb_image.h"
 #include "stb_image_resize2.h"
 #pragma warning(pop)
@@ -322,50 +323,153 @@ Image transpose_image(const Image& img)
     return result;
 }
 
-
-static int clamp(int min, int max, int value)
+static void boxblur_horizontal_noalloc(int width, int height, const uint8_t* in_pixels, uint8_t* out_pixels, int radius)
 {
-    if(value < min) return min;
-    if(value > max) return max;
-    return value;
-}
-static Image image_boxblur_linear_horizontal(const Image& img, int radius)
-{
-    uint8_t* pixels = (uint8_t*)malloc(img.width * img.height * 4);
-
-    for(int y=0; y<img.height; y++)
+    if(width <= 2*radius+1)
     {
-        uint32_t accum[4] = {};
+        // We assume the image is wide enough to contain at least one full blur window.
+        LOG_INFO("Skipping blur as image of width %d is too small to have a blur window of size %d",
+                width, 2*radius+1);
+        return;
+    }
+    assert(width > 2*radius+1);
+
+    // A box blur requires that we total up the pixel values across the blur window and
+    // divide them by the window width. This is an integer division and therefore is
+    // incredibly slow. Using libdivide speeds it up significantly on its own, but also
+    // allows us to express integer division in terms of integer additions, multiplies
+    // and shifts, which in turn allows us to apply SIMD to compute several divisions
+    // in parallel.
+    libdivide::libdivide_u16_branchfree_t divisor = libdivide::libdivide_u16_branchfree_gen(2*uint16_t(radius)+1);
+    for(int y=0; y<height; y++)
+    {
+        const uint8_t* in_row = &in_pixels[4*width*y];
+        uint8_t* out_row = &out_pixels[4*width*y];
+
+        // Prime the accumulation buffer
+        uint16_t accum[4] = {};
         for(int accum_x=-radius; accum_x<radius+1; accum_x++)
         {
-            int x = clamp(0, img.width-1, accum_x);
+            int x = max(0, min(width-1, accum_x));
             for(int c=0; c<4; c++)
             {
-                accum[c] += img.pixels[4*(img.width*y + x) + c];
+                accum[c] += in_pixels[4*(width*y + x) + c];
             }
         }
 
-        for(int x=0; x<img.width; x++)
+        // A naive implementation would need to clamp the old & new pixel X values to
+        // within the range [0,width]. This clamp is a branch and proved to be a CPU
+        // bottleneck. We eliminate that by splitting the loop across a row into 3
+        // separate loops:
+        // * One where the X is "hard coded" as "clamped to zero"
+        // * One where we assume the entire blur window falls within the valid horizontal
+        //   range. This loop covers almost all pixels.
+        // * One where X is "hard coded" as "clamped to `width-1`"
+        for(int x=0; x<radius; x++)
         {
             for(int c=0; c<4; c++)
             {
-                uint32_t val = accum[c]/(2*uint32_t(radius)+1);
-                assert(val <= 255);
-                pixels[4*(img.width*y + x) + c] = val & 0xFF;
+                uint16_t val = libdivide::libdivide_u16_branchfree_do(accum[c], &divisor);
+                out_pixels[4*(width*y + x) + c] = val & 0xFF;
             }
 
-            const int old_x = clamp(0, img.width-1, x-radius);
-            const int new_x = clamp(0, img.width-1, x+radius+1);
+            const int old_x = 0;
+            const int new_x = x+radius+1;
 
             for(int c=0; c<4; c++)
             {
-                const uint8_t oldval = img.pixels[(4*(img.width*y + old_x) + c)];
-                const uint8_t newval = img.pixels[(4*(img.width*y + new_x) + c)];
+                const uint8_t oldval = in_pixels[(4*(width*y + old_x) + c)];
+                const uint8_t newval = in_pixels[(4*(width*y + new_x) + c)];
+                accum[c] += newval;
+                accum[c] -= oldval;
+            }
+        }
+
+        const __m128i zero = {};
+        __m128i accum128 = _mm_loadu_si64(accum);
+        for(int x=radius; x<width-radius-1; x++)
+        {
+            // This is the main loop. Effectively all it needs to do is this:
+            //     for(int c=0; c<4; c++)
+            //     {
+            //         uint16_t val = libdivide::libdivide_u16_branchfree_do(accum[c], &divisor);
+            //         out_row[4*x + c] = val & 0xFF;
+            //     }
+            // however if we inline the calls to libdivide:
+            //     uint16_t libdivide_u16_branchfree_do(
+            //         uint16_t numer, const struct libdivide_u16_branchfree_t *denom) {
+            //         uint16_t q = libdivide_mullhi_u16(denom->magic, numer);
+            //         uint16_t t = ((numer - q) >> 1) + q;
+            //         return t >> denom->more;
+            //     }
+            //     static LIBDIVIDE_INLINE uint16_t libdivide_mullhi_u16(uint16_t x, uint16_t y) {
+            //         uint32_t xl = x, yl = y;
+            //         uint32_t rl = xl * yl;
+            //         return (uint16_t)(rl >> 16);
+            //     }
+            // then we can express that calculation using SIMD intrinsics and compute 4 of
+            // them in parallel, allowing us to do all 4 channels of the pixel at once.
+            __m128i magic = _mm_set1_epi16(divisor.magic);
+            __m128i numer = accum128;
+            __m128i q = _mm_mulhi_epu16(magic, numer);
+            __m128i t1 = _mm_sub_epi16(numer, q);
+            __m128i t2 = _mm_srli_epi16(t1, 1);
+            __m128i t3 = _mm_add_epi16(t2, q);
+            __m128i val = _mm_srli_epi16(t3, divisor.more);
+            __m128i packed_val = _mm_packus_epi16(val, val);
+            _mm_storeu_si64(&out_row[4*x], packed_val);
+
+            const int old_x = x - radius;
+            const int new_x = x + radius + 1;
+            const uint8_t* old_pixel_ptr = &in_row[4*old_x];
+            const uint8_t* new_pixel_ptr = &in_row[4*new_x];
+
+            // Now that we've computed the output pixel, we need to update the accumulator
+            // to remove the 'old' pixel on the left of the blur window, and add in the
+            // 'new' pixel on the right of the blur window.
+            // A naive implementation would look like this:
+            //     for(int c=0; c<4; c++)
+            //     {
+            //         accum[c] += new_pixel[c];
+            //         accum[c] -= old_pixel[c];
+            //     }
+            // as above, since this is just simple addition, we can SIMD it to do the
+            // entire accumulator at once.
+            __m128i old_pixel_packed = _mm_loadu_si32(old_pixel_ptr);
+            __m128i new_pixel_packed = _mm_loadu_si32(new_pixel_ptr);
+            __m128i old_pixel = _mm_unpacklo_epi8(old_pixel_packed, zero);
+            __m128i new_pixel = _mm_unpacklo_epi8(new_pixel_packed, zero);
+            __m128i accum_tmp = _mm_add_epi16(accum128, new_pixel);
+            accum128 = _mm_sub_epi16(accum_tmp, old_pixel);
+        }
+        _mm_storeu_si64(accum, accum128);
+
+        for(int x=width-radius-1; x<width; x++)
+        {
+            for(int c=0; c<4; c++)
+            {
+                uint16_t val = libdivide::libdivide_u16_branchfree_do(accum[c], &divisor);
+                out_pixels[4*(width*y + x) + c] = val & 0xFF;
+            }
+
+            const int old_x = x-radius;
+            const int new_x = width-1;
+
+            for(int c=0; c<4; c++)
+            {
+                const uint8_t oldval = in_pixels[(4*(width*y + old_x) + c)];
+                const uint8_t newval = in_pixels[(4*(width*y + new_x) + c)];
                 accum[c] += newval;
                 accum[c] -= oldval;
             }
         }
     }
+}
+static Image image_boxblur_linear_horizontal(const Image& img, int radius)
+{
+    uint8_t* pixels = (uint8_t*)malloc(img.width * img.height * 4);
+
+    boxblur_horizontal_noalloc(img.width, img.height, img.pixels, pixels, radius);
 
     Image result = {};
     result.width = img.width;
