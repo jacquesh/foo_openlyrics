f48711ff060531085f83fbbede77138aeed6f1b8
Jacques Heunis <git@jacquesheunis.com>
2026-01-17 18:06:38 +0000

    Add some basic terminal output to the version header generation script

--- build/generate_version_header.ps1
+++ build/generate_version_header.ps1
@@ -3,6 +3,7 @@ param (
         [string]$output_path
       )
 $version_string = git describe --tags --first-parent HEAD
+Write-Output "Computing version header from version string '$verion_string'..."
 if ([string]::IsNullOrEmpty($version_string)) {
     Write-Output "Failed to compute commit description, git returned an empty string, terminating..."
     return 1
@@ -17,3 +18,4 @@ $version_string = $version_string.Substring(1) # Remove the leading 'v'
 $output_dir = Split-Path -Parent $output_path
 New-Item -ItemType "directory" -Force $output_dir | Out-Null
 Write-Output "#pragma once`n`n#define OPENLYRICS_VERSION ""$version_string""" > $output_path
+Write-Output "Version header saved to $output_path"
